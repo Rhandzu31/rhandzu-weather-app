56ee8aaacf308915de9311b16e18a57541239e6e
Rhandzu31 <mukansi.rhandzu@gmail.com>
2024-09-20 19:52:03 +0200

    Updated weather data on index.html

--- src/index.js
+++ src/index.js
@@ -2,9 +2,41 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `$(response.data.tepmerature.humidity)%`;
+    iconElement.innerHTML = `<img src="$(response.data.condition.icon_url" class="weather-app-icon" />`;
+  }
+
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
   }
 
   function searchCity(city) {
@@ -16,6 +48,7 @@ function refreshWeather(response) {
   function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
+
     searchCity(searchInput.value);
   }
 
