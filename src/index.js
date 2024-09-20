8787aacf378d8012314437e417db9313470b6037
Rhandzu31 <mukansi.rhandzu@gmail.com>
2024-09-20 16:07:52 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function handleSearchSubmit(event) {
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+  }
+
+  function searchCity(city) {
+    let apiKey = "c9t181o33a037fabb0f43f3680067a57";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
+
+  function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
-  
+
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+  // By default, show the weather for Paris on page load
+  searchCity("Paris");
